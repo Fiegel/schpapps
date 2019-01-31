2d082dd841d7e59bc481e356f9ecd80d6aeb2c72
FIEGEL <jeremy.fiegel@hotmail.fr>
2019-01-31 20:04:40 +0100

    [css] Header and title responsive design

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../home/auth/auth.service';
@@ -8,10 +9,15 @@ import { AuthService } from '../home/auth/auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  private navbarOpen = false;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
   isAuthenticated(): boolean {
